ede3f6ea79b87e69b280a19ef3cd57f0d921383a
dpzybalov <77212554+dpzybalov@users.noreply.github.com>
2022-04-30 20:58:28 +0500

    Initial Commit

---
+++ kkk/kkkApp.swift
@@ -0,0 +1,17 @@
+//
+//  kkkApp.swift
+//  kkk
+//
+//  Created by dpzybalov on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct kkkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
